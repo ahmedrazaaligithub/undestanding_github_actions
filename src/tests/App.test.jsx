ea02c6a5a5ee6d6f713b19e27843b7ca84cfc104
AhmedDigitrends <121611274+ahmedrazaaligithub@users.noreply.github.com>
2026-07-02 19:26:25 +0500

    imported  describe, expect, it

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { describe, expect, it } from 'vitest';
 
 describe('App Component', () => {
   it('renders the application heading', () => {
